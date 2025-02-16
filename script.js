a7db94811cd15d75b9800c332ed524a3d4e60ec5
Tanishq Modi <itanishqmodi@gmail.com>
2025-02-17 01:59:07 +0530

    Make minor changes

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ console.log("Hello World");
 let x = prompt("Enter Any Number: ");
 
 y = parseInt(x);
+
+let h1 = document.createElement("h1");
+
+h1.innerText = `${y} is the number you entered.`;
+
+let body = document.querySelector("body");
+
+body.append(h1);
